0ff626235573f4a3af7b7dea60471f2c8b3f4d3c
kaylee <aussieboo0325@gmail.com>
2025-12-31 20:35:02 -0800

    one more time...

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 // copied and pasted this part
-app.use(express.static("public")); // serve your static site
+app.use(express.static(path.join(__dirname, "public")));
 
 // example API route
 app.get("/stats", cors(), async (req, res) => {
   try {
     // const slack_id = "U08QMC72ZST"
-    const startofday = dayjs().startOf('day');
-    const endofday = dayjs().endOf('day');
+    const startOfDay = dayjs().startOf("day").format("YYYY-MM-DD");
+    const endOfDay = dayjs().endOf("day").format("YYYY-MM-DD");
     const response = await fetch(`https://hackatime.hackclub.com/api/v1/users/U08QMC72ZST/stats?start_date=${startofday}&end_date=${endofday}`);
     const data = await response.json();
     res.json(data);
@@ -24,7 +24,7 @@ app.get("/stats", cors(), async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
