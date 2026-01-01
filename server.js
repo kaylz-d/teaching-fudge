d713148aeabf77a5eba401fd24dc9cec9ffa5d5c
kaylee <aussieboo0325@gmail.com>
2026-01-01 14:41:11 -0800

    nvm that kinda broke it

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors");
-const dayjs = require("dayjs")
+const cors = require ("cors");
+const dayjs = require ("dayjs")
 const path = require("path")
 const port = process.env.PORT || 3000;
 
@@ -14,9 +14,8 @@ app.use(express.static("public")); // serve your static site
 app.get("/stats", cors(), async (req, res) => {
   try {
     // const slack_id = "U08QMC72ZST"
-    const startofday = dayjs().startOf('day').format("YYYY-MM-DD");
-    const endofday = dayjs().endOf('day').format("YYYY-MM-DD");
-
+    const startofday = dayjs().startOf('day');
+    const endofday = dayjs().endOf('day');
     const response = await fetch(`https://hackatime.hackclub.com/api/v1/users/U08QMC72ZST/stats?start_date=${startofday}&end_date=${endofday}`);
     const data = await response.json();
     res.json(data);
